ef2395d5bf3a7203839666b90b0aca6711bdece9
braalex <haxball3012>
2019-11-25 19:14:11 +0300

    Add sign up page with Hibernate validation

--- src/main/java/com/nonsense/controller/MainController.java
+++ src/main/java/com/nonsense/controller/MainController.java
@@ -3,14 +3,17 @@ package com.nonsense.controller;
 import com.nonsense.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class MainController {
+
+    private static List<User> users = new ArrayList<>();
 
     @GetMapping("/view/{name}")
     public String view(@PathVariable("name") String name, Model model) {
@@ -26,11 +29,22 @@ public class MainController {
 
     @GetMapping("/users")
     public String getUsers(Model model) {
-        List<User> users = List.of(
-                new User("Tom", "Brown", "tb@mail.com"),
-                new User("Mike", "Smith", "ms@mail.com")
-        );
         model.addAttribute("users", users);
         return "/users";
+    }
+
+    @GetMapping("/users/new")
+    public String getSignUp(Model model) {
+        model.addAttribute("user", new User());
+        return "/sign-up";
+    }
+
+    @PostMapping("/users/new")
+    public String signUp(@ModelAttribute @Valid User user, BindingResult result) {
+        if (result.hasErrors()) {
+            return "/sign-up";
+        }
+        users.add(user);
+        return "redirect:/users";
     }
 }
--- src/main/java/com/nonsense/model/User.java
+++ src/main/java/com/nonsense/model/User.java
@@ -1,9 +1,22 @@
 package com.nonsense.model;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public class User {
+
+    @NotBlank(message = "Name is required")
     private String name;
+
+    @Size(min = 1, max = 20, message = "Surname should be from 1 to 20 symbols")
     private String surname;
+
+    @Email
     private String email;
+
+    public User() {
+    }
 
     public User(String name, String surname, String email) {
         this.name = name;
@@ -21,5 +34,17 @@ public class User {
 
     public String getEmail() {
         return email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
